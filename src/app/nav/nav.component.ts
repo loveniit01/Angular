a6e54e2f3b9bbd3b6bc41a47213dad19b756cb99
Pradeep Joshi <loveniit01@gmail.com>
2023-09-20 10:48:34 +0530

    different selectors in angular

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-nav',
+  // html tag,
+  // selector: 'app-nav',
+
+  // html attribute
+  // selector: '[app-nav]',
+
+  // css class
+  selector: '.app-nav',
+
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
